e4a9b619029402f434caa865722acb6b5971df3c
Harrison Liddiard <hliddiard@paypal.com>
2020-08-09 18:06:00 -0700

    change body text shadow to faux border

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,5 +6,6 @@ export default () => <Layout page="credits" pageTitle="Not Found">
   <p>Uh oh! That page doesn’t exist. Try one of the links from the navigation above.</p>
   <div style={{ width: '100%', height: 0, paddingBottom: '100%', position: 'relative' }}>
     <iframe src="https://giphy.com/embed/YyKPbc5OOTSQE" width="100%" height="100%" style={{position: 'absolute'}} frameBorder="0" className="giphy-embed" title="404" allowFullScreen />
-  </div><p><a href="https://giphy.com/gifs/gustavo-art-kidmograph-gustavo-YyKPbc5OOTSQE">via GIPHY</a></p>
+  </div>
+  <p><a href="https://giphy.com/gifs/gustavo-art-kidmograph-gustavo-YyKPbc5OOTSQE">via GIPHY</a></p>
 </Layout>
