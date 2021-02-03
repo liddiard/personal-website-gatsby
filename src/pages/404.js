bcaec14cd61bf41a8f66a3709bd689cde148d370
Harrison Liddiard <hliddiard@paypal.com>
2021-02-03 12:54:13 -0800

    update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Layout from '../components/layout'
 
 export default () => <Layout page="credits" pageTitle="Not Found">
-  <h1>404: Not Found</h1>
+  <h1>Not Found</h1>
   <p>Uh oh! That page doesn’t exist. Try one of the links from the navigation above.</p>
   <div style={{ width: '100%', height: 0, paddingBottom: '100%', position: 'relative' }}>
     <iframe src="https://giphy.com/embed/YyKPbc5OOTSQE" width="100%" height="100%" style={{position: 'absolute'}} frameBorder="0" className="giphy-embed" title="404" allowFullScreen />
