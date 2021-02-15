770ef4afd5e24c37171f113dabf5840c94799f70
Harrison Liddiard <hliddiard@paypal.com>
2021-02-15 15:05:46 -0800

    add box shadow to wrapper

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,16 +4,18 @@ import Footer from './footer'
 import Header from './header'
 
 export default ({ page, children, pageTitle, meta = {} }) => (
-  <main className={`container ${page ? page : ''}`}>
-    <Helmet>
-      <meta charSet="utf-8" />
-      <title>{pageTitle + ' | Harrison Liddiard'}</title>
-      <meta property="og:image" content={meta.image || 'https://harrisonliddiard.com/img/default_og_image.png'} />
-      {meta.description ? <meta name="description" content={meta.description} /> : null}
-    </Helmet>
-    <Header />
-    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400&family=Lora:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet" />
-    {children}
+  <>
+    <main className={`container ${page ? page : ''}`}>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>{pageTitle + ' | Harrison Liddiard'}</title>
+        <meta property="og:image" content={meta.image || 'https://harrisonliddiard.com/img/default_og_image.png'} />
+        {meta.description ? <meta name="description" content={meta.description} /> : null}
+      </Helmet>
+      <Header />
+      <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400&family=Lora:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet" />
+      {children}
+    </main>
     <Footer />
-  </main>
+  </>
 )
