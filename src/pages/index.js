cf3557b17956531d319d67152aab600732896e2c
Harrison Liddiard <omniaura5@gmail.com>
2022-09-25 13:18:26 -0400

    remove react-tag-cloud dep and upgrade all packages to latest

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,30 @@
 import React from 'react'
 import { Link, graphql } from 'gatsby'
 import { OutboundLink } from 'gatsby-plugin-google-analytics'
-import TagCloud from 'react-tag-cloud'
 import Layout from '../components/layout'
 
-const tagCloudStyle = {
-  padding: 5,
-  fontFamily: 'Lato, sans-serif',
-  fontSize: 15,
-  width: '100%',
-  height: '200px'
-}
 
 const Skills = ({ data }) => (
-  <TagCloud
-    style={tagCloudStyle}>
-    {data.allSkillsJson.edges.map(({ node }) =>
-      <div
-        key={node.id}
-        style={
-          node.type === 'software' ?
-            { color: '#6653ff', fontWeight: 'bold' } : 
-            {}
-        }>
+  <ul className="skills">
+    {data.edges.map(({ node }) =>
+      <li 
+        key={node.name} 
+        style={node.type === 'software' ? { color: '#6653ff', fontWeight: 'bold' } : {}}
+      >
         {node.name}
-      </div>)}
-  </TagCloud>
+      </li>
+    )}
+  </ul>
 );
 
 const Interests = ({ data }) => (
-  <TagCloud
-    style={{ ...tagCloudStyle, height: '80px' }}>
-    {data.allInterestsJson.edges.map(({ node }) =>
-      <div key={node.id}>{node.name}</div>)}
-  </TagCloud>
+  <ul className="interests">
+    {data.edges.map(({ node }) =>
+      <li key={node.name}>
+        {node.name}
+      </li>
+    )}
+  </ul>
 );
 
 const renderProjects = (edges) => (
@@ -61,10 +52,10 @@ const Home = ({ data }) => <Layout page="front" pageTitle="Home" meta={{ descrip
   <p className="lede">Hi! I’m Harrison, a technical product manager at <OutboundLink href="https://www.paypal.com" target="_blank" rel="noopener noreferrer">PayPal</OutboundLink> and <OutboundLink href="https://ucla.edu" target="_blank" rel="noopener noreferrer">UCLA</OutboundLink> graduate. Check out my <a href="#projects">projects</a> below and my <Link to="/resume">resumé</Link> for some things I’ve done. You can also find me on <OutboundLink href="https://linkedin.com/in/liddiard" target="_blank" rel="noopener noreferrer">LinkedIn</OutboundLink> and <OutboundLink href="https://github.com/liddiard/" target="_blank" rel="noopener noreferrer">GitHub</OutboundLink>.</p>
   <h2>Skills & Interests</h2>
   <p>Here’s some stuff I enjoy learning about and doing, to varying levels of competency. Software-specific <em>skills</em> are in <span className="primary-accent"><strong>purple</strong></span>:</p>
-  <Skills data={data} />
+  <Skills data={data.allSkillsJson} />
   <h2>Industries</h2>
   <p>I’m particularly interested in these areas of work:</p>
-  <Interests data={data} />
+  <Interests data={data.allInterestsJson} />
   <h2 id="projects">Projects</h2>
   Some things I’ve made (most recent first):
   <h3>Software</h3>
