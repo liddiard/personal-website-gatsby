96bcb300403414a169a3f53987d94ee6e66f31a0
Harrison Liddiard <hliddiard@paypal.com>
2023-08-04 15:32:53 -0400

    add blank page for smarthome

---
+++ src/pages/smarthome.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const Smarthome = () => <Layout page="smarthome" pageTitle="Smarthome">
+  <h1>Lighting setup</h1>
+  
+</Layout>
+
+export default Smarthome
