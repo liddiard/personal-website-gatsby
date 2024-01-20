dd0335b493e204392776ac7b938d73f6c59106ff
Harrison Liddiard <omniaura5@gmail.com>
2024-01-20 11:53:48 -0500

    update resume to include PM role

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -8,8 +8,8 @@ const Resume = () => <Layout page="resume" pageTitle="Resumé" meta={{ descripti
   <header className="header-resume">
     <h1>Harrison Liddiard</h1>
     <ul id="contact">
-      <li><img src="/img/phone.png" alt="phone icon" />+1 480-382-0676</li>
       <li><img src="/img/email.png" alt="email icon" />harrison@liddiard.com</li>
+      <li><img src="/img/phone.png" alt="phone icon" />(480) 382-0676</li>
       <li><img src="/img/globe.png" alt="globe icon" />harrisonliddiard.com</li>
       <li style={{ marginLeft: '-0.2em' }}><img src="/img/github.png" alt="github icon" />liddiard</li>
     </ul>
@@ -26,31 +26,29 @@ const Resume = () => <Layout page="resume" pageTitle="Resumé" meta={{ descripti
   <section className="col left">
     <h2>Work Experience</h2>
     <div className="item">
-      <h3 className="position">Software Engineer 3<span className="deemphasized"> (previously 1 & 2)</span> <span className="date">2017 – present</span></h3>
+      <h3 className="position">Technical Product Manager 3 <span className="date">2022 – present</span></h3>
       <h4 className="company">PayPal</h4>
       <ul>
-        <li>Led experiments that improved onboarding completion rate and product adoption among sellers.</li>
-        <li>Replatformed onboarding app flows to React + Node.js stack.</li>
-        <li>Won internal and external hackathons for PayPal, recruited, helped plan PayPal’s annual developer conference.</li>
-        <li>Developed program to help employees meet each other during pandemic.</li>
+        <li>Launched initiatives that lifted NNAs, revenue, and margin. Increased revenue by many-millions per year.</li>
+        <li>Working across domains to design experiments that A/B test improved user guidance in merchant onboarding & activation.</li>
       </ul>
     </div>
     <div className="item">
-      <h3 className="position">Software Engineering Intern <span className="date">Summer 2016</span></h3>
+      <h3 className="position">Software Engineer 1–3 <span className="date">2017 – 2022</span></h3>
+      <h4 className="company">PayPal</h4>
+      <ul>
+        <li>Led experiment implementations on Node.js + React stack that improved onboarding completion rate and product adoption among sellers.</li>
+        <li>Won internal and external hackathons for PayPal, recruited, helped plan PayPal’s annual developer conference.</li>
+        <li>Developed software that helped 100s of employees meet each other during Covid WFH.</li>
+      </ul>
+    </div>
+    <div className="item">
+      <h3 className="position">Software Engineering Intern <span className="date">2016</span></h3>
       <h4 className="company">The New York Times</h4>
       <ul>
         <li>Increased CMS search speed 10x with Elasticsearch.</li>
         <li>Search frontend: reduced complexity, redesigned results display, implemented as-you-type feedback for queries.</li>
         <li>Wrote binary search typographic headline balancer that runs on all nytimes.com article pages.</li>
-      </ul>
-    </div>
-    <div className="item">
-      <h3 className="position">hackNY Fellow, Software Engineering Intern <span className="date">Summer 2015</span></h3>
-      <h4 className="company">Wyng</h4>
-      <ul>
-        <li>Fellow for hackNY mentorship program which teaches in the intersection between civics and technology, led by Columbia and NYU faculty.</li>
-        <li>Developed full-stack with React, Django, and MongoDB.</li>
-        <li>Participated in user-driven product design with PMs, designers, and engineers to plan and scope product functionality.</li>
       </ul>
     </div>
     <div className="item">
@@ -63,7 +61,7 @@ const Resume = () => <Layout page="resume" pageTitle="Resumé" meta={{ descripti
       </ul>
     </div>
     <div className="item">
-      <h3 className="position">Managing Editor, Digital <span className="date">2014 – 2015</span></h3>
+      <h3 className="position">Managing Editor: Digital <span className="date">2014 – 2015</span></h3>
       <h4 className="company">Daily Bruin</h4>
       <ul>
         <li>Taught and advised 80+ people in web development, videography, and podcasting at UCLA’s student news organization.</li>
@@ -75,36 +73,32 @@ const Resume = () => <Layout page="resume" pageTitle="Resumé" meta={{ descripti
     <h2>Education</h2>
     <div className="item">
       <h3 className="school">UCLA <span className="date">2017</span></h3>
-      B.A., Computer Science and Linguistics
+      Computer Science and Linguistics, B.A.
     </div>
+    <div className="tech-skills">
     <h2>Technical Skills</h2>
-    <ul className="skills item">
-      <li>JavaScript <span className="deemphasized">(experienced)</span></li>
-      <li>React <span className="deemphasized">(experienced)</span></li>
-      <li>Node.js <span className="deemphasized">(experienced)</span></li>
-      <li>GraphQL <span className="deemphasized">(familiar)</span></li>
-      <li>Python <span className="deemphasized">(familiar)</span></li>
-      <li>Django, Flask <span className="deemphasized">(familiar)</span></li>
-      <li>HTML, CSS <span className="deemphasized">(experienced)</span></li>
-      <li>UI design <span className="deemphasized">(familiar)</span></li>
-    </ul>
+      <h3>Experienced <span className="deemphasized">(5+ years)</span></h3>
+      <p className="item">JavaScript, React, Node.js, HTML & CSS, Python, Django, API design</p>
+      <h3>Familiar <span className="deemphasized">(1–5 years)</span></h3>
+      <p className="item">TypeScript, Next.js, SQL, UX design</p>
+    </div>
     <h2>Projects</h2>
     <ul className="projects item">
-      <li><span className="project-name">Slack Meetups</span>: pair people in a Slack channel for 1:1 meetups</li>
+      <li><span className="project-name">Stream Editor</span>: run Unix text manipulation commands interactively on the web</li>
+      <li><span className="project-name">Weather/transit board</span>: colorful, informative LED fridge magnet using HTML Canvas</li>
+      <li><span className="project-name">Slack Meetups</span>: pair people in a channel for 1:1s and generate visual reports</li>
       <li><span className="project-name">google-sheet-s3</span>: instantly create an API from a spreadsheet</li>
-      <li><span className="project-name">Stream Editor</span>: use Unix text manipulation commands interactively on the web</li>
-      <li><span className="project-name">MailPDF</span>: physically mail a digital document in under 60 sec</li>
       <li><span className="project-name">kikl.co</span>: shorten links with easy-to-remember URLs</li>
     </ul>
     <h2>Other Skills & Achievements</h2>
     <ul className="skills item">
       <li>FAA-licensed private aircraft pilot & commercial drone operator</li>
       <li>PADI-certified scuba diver</li>
-      <li>Eagle Scout with Bronze Palm</li>
       <li>Black belt in Kenpo</li>
+      <li>Eagle Scout</li>
       <li>Yamaha Music School graduate</li>
-      <li>Event planner</li>
-      <li>Photo, audio, video editor</li>
+      <li>Technical communicator</li>
+      <li>Photo, audio & video editor</li>
     </ul>
   </section>
   <div className="more">
