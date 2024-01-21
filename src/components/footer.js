67739f752e1d5e9e102adedb22dfb9049e838fa4
Harrison Liddiard <omniaura5@gmail.com>
2024-01-21 15:08:17 -0500

    footer punctuation

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 const Footer = () => <footer>
-  Content © {new Date().getFullYear()} Harrison Liddiard – All rights reserved. Product names, logos, and brands are property of their respective owners. <Link to="/credits">Background image credits</Link>.
+  Content © {new Date().getFullYear()} Harrison Liddiard. All rights reserved. Product names, logos, and brands are property of their respective owners. <Link to="/credits">Background image credits</Link>.
 </footer>
 
 export default Footer
