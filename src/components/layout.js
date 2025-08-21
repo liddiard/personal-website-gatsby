9091e6a2967420bb16acf206ba51d0f1c61a8ec2
Harrison Liddiard <git@harrisonliddiard.com>
2025-08-20 20:17:32 -0400

    fix crossOrigin attribute reference

--- src/components/layout.js
+++ src/components/layout.js
@@ -14,7 +14,7 @@ const Layout = ({ page = '', children, pageTitle, className = '', meta = {} }) =
       </Helmet>
       <Header />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       <link href="https://fonts.googleapis.com/css2?family=Gabarito:wght@400..900&family=Lexend:wght@100..900&display=swap" rel="stylesheet" />
       {children}
     </main>
